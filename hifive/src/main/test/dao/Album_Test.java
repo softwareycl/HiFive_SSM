a4cb8005eaf55b0449fe7bca9edd6a130bd94627
GuoYunZheSE <guoyunzhe.SE@gmail.com>
2018-06-21 15:20:12 +0800

    Album:select, selectAllSongs

--- hifive/src/main/test/dao/Album_Test.java
+++ hifive/src/main/test/dao/Album_Test.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import base.BaseTest;
 
+import java.util.List;
+
+import org.junit.Test;
+
 import com.musicweb.dao.AlbumDao;
 import com.musicweb.domain.Album;
+import com.musicweb.domain.Song;
 public class Album_Test extends BaseTest{
 	@Autowired
 	AlbumDao albummapper;
-
+	
 //	@Test
 	public void test_selectByCategory() {
 		try {
@@ -23,8 +28,24 @@ public class Album_Test extends BaseTest{
 //	@Test
 	public void test_select() {
 		try {
-			albummapper.select(1);
+			Album album = albummapper.select(6);
 			System.out.println("test_select succeed");
+			System.out.println(album.getArtistName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void test_selectAllSongs() {
+		try {
+			List<Song> l = albummapper.selectAllSongs(1);
+			System.out.println("test_select succeed");
+			for(Song s : l) {
+				System.out.println(s.getName());
+				System.out.println(s.getAlbumName());
+			}
+				
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
