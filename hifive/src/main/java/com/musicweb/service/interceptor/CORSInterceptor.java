05aab2f0ace37abce2b5c461949a493c73addd3a
zhanghuakui <1033185507@qq.com>
2018-06-27 13:34:05 +0800

    使得跨域访问

--- hifive/src/main/java/com/musicweb/service/interceptor/CORSInterceptor.java
+++ hifive/src/main/java/com/musicweb/service/interceptor/CORSInterceptor.java
@@ -3,6 +3,7 @@ package com.musicweb.service.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,21 +12,17 @@ public class CORSInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
 		response.addHeader("Access-Control-Allow-Origin", "*");
-
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-
 	}
 }
